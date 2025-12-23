9a17b28c7372152054e00029f0f967a79274cff6
mainframe <kaiser@mainframe.local>
2025-12-23 08:33:38 +0300

    test nuxthub migration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  srcDir: 'app',
-  compatibilityDate: '2025-07-15',
+  srcDir: "app",
+  compatibilityDate: "2025-12-23",
   devtools: { enabled: true },
-  modules: ['@nuxt/ui','motion-v/nuxt'],
-  css: ['~/assets/css/main.css']
-})
+  modules: ["@nuxt/ui", "motion-v/nuxt"],
+  css: ["~/assets/css/main.css"],
+  nitro: {
+    preset: "cloudflare_module",
+    cloudflare: {
+      deployConfig: true,
+      nodeCompat: true,
+    },
+  },
+});
